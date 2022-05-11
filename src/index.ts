ef69591da063d11e01f0d238f90f662b512c4df7
William <william@evictionapp.com>
2022-05-11 11:25:53 -0500

    auto deploy

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
 import { $ } from "../extension-src/utils";
 import Odyssey from "./Odyssey";
 
+interface Counties {
+  [key: string]: RegExp;
+}
+
+const counties: Counties = {
+  harris: /^[0-9]+$/,
+  tarrant: /^JP[0-9]+-[0-9]+-E.+$/,
+  fortbend: /^[0-9]+-JEV[0-9]+-[0-9]+$/,
+};
+
 const getCounty = (caseNumber: string): string => {
   const countyEl = $<HTMLInputElement>("county");
   if (countyEl.value) {
     return countyEl.value.toLowerCase();
   }
-  if (/^[0-9]+$/.test(caseNumber)) {
-    return "harris";
-  }
-  if (/^JP[0-9]+-[0-9]+-E.+$/.test(caseNumber)) {
-    return "tarrant";
+  for (const county in counties) {
+    const re = counties[county];
+    if (re.test(caseNumber)) {
+      return county;
+    }
   }
 
   throw new Error(`Could not recognize county for case number ${caseNumber}`);
+};
+
+const state = {
+  caseNumber: "",
+  county: "",
 };
 
 const renderPage = async () => {
@@ -22,15 +37,21 @@ const renderPage = async () => {
     return;
   }
   const rootEl = $("root");
-  rootEl.innerHTML = "";
-
-  const loadingNode = document.createElement("div");
-  loadingNode.className = "m-4 font-extrabold text-gray-700";
-  loadingNode.innerText = "Loading...";
-  rootEl.appendChild(loadingNode);
 
   try {
     const county = getCounty(caseNumber);
+    if (county === state.county && caseNumber === state.caseNumber) {
+      return;
+    }
+    state.caseNumber = caseNumber;
+    state.county = county;
+    history.pushState({}, "", `?caseNumber=${caseNumber}&county=${county}`);
+
+    const loadingNode = document.createElement("div");
+    loadingNode.className = "m-4 font-extrabold text-gray-700";
+    loadingNode.innerText = "Loading...";
+    rootEl.innerHTML = "";
+    rootEl.appendChild(loadingNode);
 
     const url = `https://ihaucbf8v2.execute-api.us-east-1.amazonaws.com/${county}?caseNumber=${caseNumber}`;
     const res = await fetch(url).then(async (res) => {
@@ -65,12 +86,14 @@ const main = () => {
   customElements.define("odyssey-html", Odyssey);
 
   const caseNumberEl = $<HTMLInputElement>("case-number");
+  caseNumberEl.onblur = () => renderPage();
   caseNumberEl.onkeydown = (ev) => {
-    ev.key === "Enter" && renderPage();
+    ev.key === "Enter" && caseNumberEl.blur();
   };
   const countyEl = $<HTMLInputElement>("county");
+  countyEl.onblur = () => renderPage();
   countyEl.onkeydown = (ev) => {
-    ev.key === "Enter" && caseNumberEl.value.length > 0 && renderPage();
+    ev.key === "Enter" && caseNumberEl.value.length > 0 && countyEl.blur();
   };
 
   const caseNumber = new URLSearchParams(window.location.search).get(
